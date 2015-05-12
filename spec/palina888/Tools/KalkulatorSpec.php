3301668b535ed7dc87ca83b2f22f639dab2a0474
palina888 <palina888@o2.pl>
2015-05-12 18:59:10 +0200

    palina888/kalkulator: settery i gettery

--- spec/palina888/Tools/KalkulatorSpec.php
+++ spec/palina888/Tools/KalkulatorSpec.php
@@ -7,8 +7,18 @@ use Prophecy\Argument;
 
 class KalkulatorSpec extends ObjectBehavior
 {
-    function it_is_initializable()
+     function it_is_initializable()
     {
         $this->shouldHaveType('palina888\Tools\Kalkulator');
+    }
+    
+      function it_should_have_setter_and_gettter()
+    {
+        $this->setA(123)->getA()->shouldReturn(123);
+        $this->setB(987)->getB()->shouldReturn(987);
+   }
+   function it_should_calculate_add()
+    {
+        $this->setA(5)->setB(7)->add()->shouldReturn(12);
     }
 }
